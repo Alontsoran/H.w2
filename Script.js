ed528a7b21a2325e932d28d090435aedddfd34b8
AlonTsoran <alontsoranschool@gmail.com>
2024-01-28 23:15:12 +0200

    m

--- Script.js
+++ Script.js
@@ -1,25 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var resumeButton = document.getElementById('RESUME');
-    var resumeClicked = false;
-
-    resumeButton.addEventListener('click', function () {
-        if (!resumeClicked) {
-            // Set the flag to prevent multiple clicks
-            resumeClicked = true;
-
-            // Create a hidden link
-            var downloadLink = document.createElement('a');
-            downloadLink.href = './Photos/קורות חיים 2024 .pdf';
-            downloadLink.download = 'Resume.pdf';
-
-            // Append the link to the body
-            document.body.appendChild(downloadLink);
-
-            // Trigger a click on the link
-            downloadLink.click();
-
-            // Remove the link from the DOM
-            document.body.removeChild(downloadLink);
-        }
-    });
-});
+function toggleNavigation() {
+    var menu = document.querySelector('.menu');
+    menu.style.display = menu.style.display === 'none' ? 'flex' : 'none';
+}
