d97a69223defa31eab198ac2d72b815f3d30796d
AlonTsoran <alontsoranschool@gmail.com>
2024-01-28 19:01:21 +0200

    sadsadadsasdadssadsad
    afda

--- Script.js
+++ Script.js
@@ -1,1 +1,25 @@
-const Downloadresume=document.getElementById("re");
+document.addEventListener('DOMContentLoaded', function () {
+    var resumeButton = document.getElementById('RESUME');
+    var resumeClicked = false;
+
+    resumeButton.addEventListener('click', function () {
+        if (!resumeClicked) {
+            // Set the flag to prevent multiple clicks
+            resumeClicked = true;
+
+            // Create a hidden link
+            var downloadLink = document.createElement('a');
+            downloadLink.href = './Photos/קורות חיים 2024 .pdf';
+            downloadLink.download = 'Resume.pdf';
+
+            // Append the link to the body
+            document.body.appendChild(downloadLink);
+
+            // Trigger a click on the link
+            downloadLink.click();
+
+            // Remove the link from the DOM
+            document.body.removeChild(downloadLink);
+        }
+    });
+});
