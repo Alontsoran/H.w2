9abcd79c0e54386c7914c021977cffb2144eaa23
AlonTsoran <alontsoranschool@gmail.com>
2024-02-01 08:34:37 +0200

    updates

--- Script.js
+++ Script.js
@@ -2,13 +2,3 @@ function toggleNavigation() {
     var menu = document.querySelector('.menu');
     menu.style.display = menu.style.display === 'none' ? 'flex' : 'none';
 }
-const pageButtons = document.querySelectorAll('.menu'); 
-const correctPage = window.location.pathname;
-debugger
-
-pageButtons.forEach(thisLink => {
-    const thisLinkPath = new URL(thisLink.querySelector('a').href).pathname;
-    if (correctPage === thisLinkPath) {
-        thisLink.classList.add('active');
-    }
-});
