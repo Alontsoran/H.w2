daa1b8b226fb9a1d250ad5d41b1e654db1e65458
AlonTsoran <alontsoranschool@gmail.com>
2024-02-04 09:23:31 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,11 +1,7 @@
- 
-    function toggleNavigation() {
-        var menu = document.querySelector('.menu');
-        menu.style.display = menu.style.display === 'none' ? 'flex' : 'none';
-    }
+function toggleNavigation() {
+  var menu = document.querySelector(".menu");
+  menu.style.display = menu.style.display === "none" ? "flex" : "none";
+}
 
-    const mobilenavbar = document.getElementById("menuPhoto");
-    mobilenavbar.addEventListener("click", toggleNavigation);
-    
-   
-       
+const mobilenavbar = document.getElementById("menuPhoto");
+mobilenavbar.addEventListener("click", toggleNavigation);
